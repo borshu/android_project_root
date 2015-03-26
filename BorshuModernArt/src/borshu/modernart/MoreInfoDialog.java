1707d1886365615e50d973e9cae9f30255c87e3e
bshoorin <borshu@gmail.com>
2015-03-26 14:55:40 +0200

    commented code removed, unused import removed

--- BorshuModernArt/src/borshu/modernart/MoreInfoDialog.java
+++ BorshuModernArt/src/borshu/modernart/MoreInfoDialog.java
@@ -1,15 +1,16 @@
 package borshu.modernart;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MoreInfoDialog extends DialogFragment {
 	
+	@SuppressLint("InflateParams")
 	@Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 		
@@ -22,15 +23,6 @@ public class MoreInfoDialog extends DialogFragment {
         builder.setCancelable(true);
         
         final AlertDialog dialog = builder.create(); 
-
-//        Button btn = (Button) dialog.findViewById(R.id.btnNotNow);
-//        
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                dialog.cancel();
-//            }
-//        });
-
         
         return dialog;
     }
