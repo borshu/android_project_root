2650df0c741c63dc6a26b9f6afb4c795f444a4df
bshoorin <borshu@gmail.com>
2015-03-29 20:24:02 +0300

    not used imports are removed

--- AsyncTask/Lab5-AsyncTaskLab/src/course/labs/asynctasklab/DownloaderTaskFragment.java
+++ AsyncTask/Lab5-AsyncTaskLab/src/course/labs/asynctasklab/DownloaderTaskFragment.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 import android.app.Activity;
 import android.app.Fragment;
